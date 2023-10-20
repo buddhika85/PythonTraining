265b99d2d94eccf777fe0ceaf7434b3dbc542c4d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-20 21:19:11 +0530

    while loop - guessing game

--- basics/16_random_num_geuss_game.py
+++ basics/16_random_num_geuss_game.py
@@ -3,14 +3,14 @@ import random
 random_number = random.randrange(1, 5)      # it can be 1,2,3,4
 #print(f'Testing: Random number is {random_number}')
 max_guess_count = 3
-i = 0
+guess_count = 1
 is_won = False;
-while not is_won and  i < max_guess_count:
-    guess = int(input(f'Guess {i + 1}: '))
+while not is_won and  guess_count <= max_guess_count:
+    guess = int(input(f'Guess {guess_count}: '))
     if guess == random_number:
         print('You win !')
         is_won = True
-    i += 1
+    guess_count += 1
 
 if not is_won:
     print(f'You lost. Random number was {random_number}. Better luck next time !')
