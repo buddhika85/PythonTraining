1cdcad8a3d78ffbfbca54a18a8668907da86c074
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-20 18:36:49 +0530

    if statements - excercise

--- basics/11_if_excercise.py
+++ basics/11_if_excercise.py
@@ -1,7 +1,7 @@
 price_of_house = 1000000
-is_good_credit = False
+is_good_credit = True
 if is_good_credit:
-    price_of_house = (price_of_house / 100) * 90
+    down_payment = (price_of_house / 100) * 10
 else:
-    price_of_house = (price_of_house / 100) * 80
-print(price_of_house)
+    down_payment = (price_of_house / 100) * 20
+print(f'Down payment: ${down_payment}')
