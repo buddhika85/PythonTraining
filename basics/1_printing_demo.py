d2604eb09283257a0ade66ce70eb0edee7004d45
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-20 14:28:31 +0530

    user input reading

---
+++ basics/1_printing_demo.py
@@ -0,0 +1,20 @@
+print("Buddhika Gunasekera")
+print('*' * 10)
+price = 10
+price = 20.0
+print(price)
+
+price = 'x'
+print(price)
+
+is_published = False
+print(is_published)
+
+full_name = "John Smith"
+age = 20
+is_new = True
+# if (is_new)
+#     is_new_str = "is new patient"
+# else
+#     is_new_str = "existing patient"
+print(f"{full_name} is {age} and is new {is_new}")
---
+++ basics/2_user_input.py
@@ -0,0 +1,3 @@
+name = input('what is your name ? ')
+fav_color = input('what is your favourite color ? ')
+print(f"Hi {name}, you like {fav_color}")
