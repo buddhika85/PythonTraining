43be84c74b63ae6446c7e5424a1f22c8a8faac94
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-20 18:15:52 +0530

    math functions

---
+++ basics/9_math_functions.py
@@ -0,0 +1,19 @@
+x = 2.9
+print(round(x))
+
+x = 2.4
+print(round(x))
+
+x = 2.5
+print(round(x))
+
+x = 2.51
+print(round(x))
+
+x = -1.12
+print(abs(x))
+
+import math
+
+print(math.ceil(2.1))
+print(math.floor(2.9))
