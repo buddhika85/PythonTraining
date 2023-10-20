6ea16cf93f4023456a32ac504309c54a8328b104
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-20 15:55:43 +0530

    string functions

---
+++ basics/6_string_functions.py
@@ -0,0 +1,11 @@
+name = 'Python for beginners'
+
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.find('t'))
+print(name.find('T'))
+
+replaced_name  = name.replace('P', 'J')
+print(replaced_name)
+print('Jython' in replaced_name)
