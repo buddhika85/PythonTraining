c1d57dc705ca22ed9a4faad05eff84065be7d9df
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-22 21:57:40 +0530

    for loop

---
+++ 18_for_loop.py
@@ -0,0 +1,35 @@
+for item in 'Python':
+    print(item)
+
+print()
+
+for item in [1,2,3,4,5]:
+    print(item)
+
+print()
+
+for item in ['jack', 'gill', 'john']:
+    print(item)
+
+print()
+
+list = ['A', 'B', 'C']
+for item in list:
+    print(item)
+
+print()
+
+# range 
+for item in range(5):       # 0,1,2,3,4
+    print(item)
+
+print()
+
+# range 
+for item in range(5,10):       # 5,6,7,8,9
+    print(item)
+
+print()
+
+for item in range(5,10,2):       # 3 arg is step - 5,7,9
+    print(item)
