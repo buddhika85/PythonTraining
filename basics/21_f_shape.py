e971ae0709cd336e248700939a7386cc5068d371
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-23 01:17:49 +0530

    nested for loops - refactor snake casing

--- basics/21_f_shape.py
+++ basics/21_f_shape.py
@@ -1,15 +1,15 @@
-xCounts = [5, 2, 5, 2, 2]
+x_counts = [5, 2, 5, 2, 2]
 
-# for lineXCount in xCounts:
-#     xCount = 0
-#     lineStr = ''
-#     while xCount < lineXCount:
-#         lineStr += 'x'
-#         xCount += 1
-#     print(lineStr)
+# for line_x_count in x_counts:
+#     x_count = 0
+#     line_str = ''
+#     while xCount < line_x_count:
+#         line_str += 'x'
+#         x_count += 1
+#     print(line_str)
 
-for lineXCount in xCounts:    
-    lineStr = ''
-    for x in range(0, lineXCount):
-        lineStr += 'x'
-    print(lineStr)
+for line_x_count in x_counts:    
+    line_str = ''
+    for x in range(0, line_x_count):
+        line_str += 'x'
+    print(line_str)
