c188c061303a28f29d132cb995d4bdb45a06151f
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-22 21:48:24 +0530

    car game next level

--- basics/17_car_game.py
+++ basics/17_car_game.py
@@ -1,12 +1,25 @@
 play = True
+prev_command = ''
 while(play):
-    command = input('>').lower()
-    if command == 'start':
-        print('Car started...Ready to go!')
-    elif command == 'stop':
-        print('Car stopped.')
+    command = input('> ').lower()
+    if command == 'start' and prev_command != command:
+        print('Car started...Ready to go!\n')
+    elif command == 'start' and prev_command == command:
+        print('Car is already started, cannot start again !!!\n')
+    elif command == 'stop' and prev_command != command:
+        print('Car stopped.\n')
+    elif command == 'stop' and prev_command == command:
+        print('Car is already stopped, cannot steop again !!!\n')
+    elif command == 'help':
+        print("""
+        start - to start the car
+        stop - to stop the car
+        quit - to quit
+        help - to see instructions\n
+        """)
     elif command == 'quit':
         print()
         play = False
     else:
-        print("I don't understand that")
+        print("I don't understand that\n")
+    prev_command = command
