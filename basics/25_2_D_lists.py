b84920a7e5ca2d41a451d02e315fff4df87349ac
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-23 15:34:31 +0530

    2 D lists

---
+++ basics/25_2_D_lists.py
@@ -0,0 +1,16 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+# printing all
+# for array in matrix:
+#     for num in array:
+#         print(num)
+
+# access one array
+print(matrix[1])
+
+# access one element of an array
+print(matrix[1][2])
