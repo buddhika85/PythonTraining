40a4b657db6581a97247520d35a6d8752743fe39
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-23 16:00:28 +0530

    list functions

---
+++ basics/26_list_methods.py
@@ -0,0 +1,37 @@
+numbers = [10,20,30,40]
+print(numbers)
+
+numbers.append(41)      # add 41 at the end
+print(numbers)
+
+numbers.insert(1,11)    # to index 1  insert 11
+print(numbers)
+
+numbers.remove(40)
+print(numbers)
+
+# numbers.clear()         # remove all
+# print(numbers)
+
+print(numbers.pop())        # removes last and returns
+print(numbers)
+
+print(numbers.index(20))  # index of value 20
+
+print(50 in numbers)        # exitance of an element
+
+numbers.insert(0,20)
+print(numbers)
+print(numbers.count(20))    # count of elements with 20
+
+numbers.sort()
+print(numbers)
+
+numbers.reverse()
+print(numbers)
+
+numbersTwo = numbers.copy()     # make a copy
+numbersTwo.sort()               # sort only the copy
+print(numbersTwo)
+
+print(numbers)                  # original is unsorted, only copy got sorted
