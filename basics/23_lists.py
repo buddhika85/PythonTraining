4c1f263809c79a12cb556816b56de5025426c1af
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-23 15:22:05 +0530

    lists basics

---
+++ basics/23_lists.py
@@ -0,0 +1,18 @@
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mary']
+
+# print(names)
+# print(names[0]) 
+# print(names[-0])  # same as 0
+# print(names[1])
+# print(names[-1]) # last one
+# print(names[-2]) # one before last one
+
+# range of items
+# print(names[1:])   # index 1 to last
+print(names[1:3])   # index 1 to 3 - 1
+
+# modify
+names [2] = 'Mosh H'
+print(names)
+
+
