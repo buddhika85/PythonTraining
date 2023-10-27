a6a785e44f543223f3b61da562948339f88c93ca
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-28 05:00:59 +0530

    remove duplicates in a list

---
+++ basics/27_remove_duplicates_list.py
@@ -0,0 +1,13 @@
+list = [1,1,2,1,3,3,4,1,3,5,6,4]
+print(f'\nOriginal: {list}')
+
+list.sort();
+print(f'\nSorted: {list}')
+i = 1
+while i < len(list):
+    if list[i] == list[i - 1]:
+        list.remove(list[i])
+    else:
+        i += 1
+
+print(f'\nRemoved Duplicates: {list}')
