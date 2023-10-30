3e552e3ce957bf6b17faf89ac424ff3f12bf7dfe
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-31 05:21:23 +0530

    keyword arguments

--- basics/33_functions.py
+++ basics/33_functions.py
@@ -1,9 +1,13 @@
 # def keyword is used
 #define function before you call them
-def greet_user(name):
-    print(f'Hi {name}!')
-    print('Welcome abroad')
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome abroad\n')
 
 print('Start----')
-greet_user('John')
+greet_user('John', 'Smith')
+
+greet_user(first_name='Gill', last_name='Smith')    # keyword arguments improve readability 
+greet_user(last_name='Ponting', first_name='Ricky')    # keyword arguments change order of arguments
+
 print('End----')
