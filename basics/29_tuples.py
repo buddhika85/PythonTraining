15203a49c44491824e66949fe42872a43f22805c
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-30 15:11:55 +0530

    tuples

---
+++ basics/29_tuples.py
@@ -0,0 +1,6 @@
+# Tuples are immutable structures - cannot assign, change once created
+
+numbers = (1,2,3)       # lists uses [], Tuples uses ()
+print(numbers)
+print(numbers[0])
+# numbers[0] = 0        # this will not work, as Tuples are immutable, cannot change them            
