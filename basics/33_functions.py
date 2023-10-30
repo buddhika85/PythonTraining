a0ba1379c497d5a725f9ec08a883619cb2fa5d24
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-30 20:54:54 +0530

    functions

---
+++ basics/33_functions.py
@@ -0,0 +1,9 @@
+# def keyword is used
+#define function before you call them
+def greet_user():
+    print('Hi there!')
+    print('Welcome abroad')
+
+print('Start----')
+greet_user()
+print('End----')
