1b2ebd7f40541dcb2443b45f2850733e7d2aa737
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-30 15:19:38 +0530

    unpakcing - with tuples and lists

---
+++ basics/30_unpacking.py
@@ -0,0 +1,24 @@
+numbers = (1,2,3)
+
+# unpacking - assigning 3 elements into 3 varaibles
+x, y, z = numbers;
+
+print(x)
+print(y)
+print(z)
+print(f'Total is {x +  y +  z}')
+
+
+
+# unpacking with lists
+numbers = [1,2,3,4]
+
+# unpacking - assigning 3 elements into 3 varaibles
+x, y, z, p = numbers;
+
+print(x)
+print(y)
+print(z)
+print(p)
+
+print(f'Total is {x +  y +  z + p}')
