11f13cd56eed15e1a1161842306d3f66e2de6991
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-11-03 19:49:19 +0530

    functions param passing and return value

--- basics/34_functions_with_return.py
+++ basics/34_functions_with_return.py
@@ -1,5 +1,13 @@
-def square(num):
-    return num * num;
+def square(num, power):
+    result = 1
+    i = 1
+    while i <= power:
+        result *= num
+        i += 1
+    return result
 
-num = 2
-print(f'{num} ^ 2 = {square(num)}')
+# by default all functions return NONE, its like null in Java
+
+num = int(input('Number to Sqaure:\n'))
+power = int(input('Number to Sqaure:\n') )
+print(f'{num} ^ {power} = {square(num, power)}')
