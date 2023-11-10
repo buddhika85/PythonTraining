8b5cf509b96381346152e507958c05a8931c414a
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-11-10 15:12:09 +0530

    excpetion handling - try except

---
+++ basics/35_try_except.py
@@ -0,0 +1,10 @@
+# exception handling
+try:
+    age = int(input('Age : '))
+    income =20000
+    risk = income / age
+    print(age)
+except ValueError:
+    print('Error : Invalid value')
+except ZeroDivisionError: 
+    print('Error : ZeroDivisionError - cannot devide by zero, age input cannot be zero')
