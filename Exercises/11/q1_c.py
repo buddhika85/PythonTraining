fe049dbaf486043e54d9d51ae3fb4c37bfb707bb
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-12-31 22:05:08 +0530

    q1

--- Exercises/11/q1_c.py
+++ Exercises/11/q1_c.py
@@ -4,3 +4,11 @@ for num in range(34, 58):
     #print(num)
     total += num
 print(f"Total => {total}")
+
+# testing with a while loop
+total = 0
+num = 34
+while(num <= 57):
+    total += num
+    num += 1
+print(f"Total from while => {total}")
