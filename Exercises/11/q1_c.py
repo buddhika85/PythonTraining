f7c27574f6144d331405908ce48f3bea40600678
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-12-31 22:03:09 +0530

    q1

---
+++ Exercises/11/q1_c.py
@@ -0,0 +1,6 @@
+# Write a Python loop that uses the range function to add up all numbers from 34 up to and including 57.
+total = 0
+for num in range(34, 58):
+    #print(num)
+    total += num
+print(f"Total => {total}")
