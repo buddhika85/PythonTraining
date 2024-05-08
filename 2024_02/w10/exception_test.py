305b4083eb35b5492a04b045449080bab139efdd
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-05-08 20:49:36 +0530

    python exceptions

---
+++ 2024_02/w10/exception_test.py
@@ -0,0 +1,14 @@
+max_retry_count = 3
+current = 0
+input_read = False
+while current < max_retry_count and not input_read:
+    current += 1
+    try:
+        number = int(input(f'Attempt {current} - Please enter a number: '))
+        input_read = True
+    except:
+        print('\tError - Please provide a valid number')
+if input_read:
+    print(f'\nYour input {number}')   
+else:
+    print(f'\nYou were unable to give a valid number with in {max_retry_count} attempts, Im giving up.')     
