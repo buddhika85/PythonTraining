ef5aeb972daec1f22349934fd0683b697534dbf0
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-05-09 01:00:12 +0530

    TA start

---
+++ 2024_02/w10/GradeCalculator.java
@@ -0,0 +1,13 @@
+public class GradeCalculator {
+
+}
+
+class SubjectGrade {
+    String name;
+    double grade;
+
+    SubjectGrade(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+}
